5e85652f0635d81b166c8dacca795dba6cf323e8
gamerslink <100190888+gamerslink@users.noreply.github.com>
2023-02-07 23:11:38 +0800

    init

---
+++ src/main/java/com/liaojacky/practise_boot_jpa/PractiseBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.liaojacky.practise_boot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PractiseBootJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liaojacky/practise_boot_jpa/PractiseBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaojacky.practise_boot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
